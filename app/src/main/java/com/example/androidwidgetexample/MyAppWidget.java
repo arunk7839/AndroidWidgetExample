cf3457b760e22649e2449ff52c3698a94b9c7f75
Arun Chandravanshi <arunk7839@gmail.com>
2020-02-04 15:07:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidwidgetexample/MyAppWidget.java
@@ -0,0 +1,46 @@
+package com.example.androidwidgetexample;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MyAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+       // Construct an Intent object includes web adresss.
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://c1ctech.com"));
+
+        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_app_widget);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+
+    }
+
+
+}
+
